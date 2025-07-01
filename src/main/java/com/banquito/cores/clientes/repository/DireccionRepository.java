8f9d95098fa972a979dc4593c7651c0fba9e1740
Freddy Leonel Pachacama <flpachacama@espe.edu.ec>
2025-06-30 19:20:37 -0500

    Upload

--- src/main/java/com/banquito/cores/clientes/repository/DireccionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.banquito.cores.clientes.repository;
-
-import com.banquito.cores.clientes.model.Direccion;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface DireccionRepository extends MongoRepository<Direccion, String> {
-}
--- src/main/java/com/banquito/cores/clientes/repository/NumeroTelefonicoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.banquito.cores.clientes.repository;
-
-import com.banquito.cores.clientes.model.NumeroTelefonico;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface NumeroTelefonicoRepository extends MongoRepository<NumeroTelefonico, String> {
-}
